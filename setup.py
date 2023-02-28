edc3789f082fdbaf288658e92ddf3a6a775c53c1
Farkhod Sadykov <farkhod.sadykov@gmail.com>
2023-02-28 02:26:31 +0100

    Added PowerShell support, minor prompt improvements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # pylint: disable=consider-using-with
 setup(
     name="shell_gpt",
-    version="0.5.0",
+    version="0.5.1",
     packages=find_packages(),
     py_modules=[
         "sgpt",
