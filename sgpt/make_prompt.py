a88c7f0d8d740e6d25631c2328516e8e14ca2acc
desiredState <113993892+des1redState@users.noreply.github.com>
2023-04-06 13:08:32 +0100

    Fix typo in make_prompt.py
    Fix typo in make_prompt.py

--- sgpt/make_prompt.py
+++ sgpt/make_prompt.py
@@ -16,7 +16,7 @@ Command:"""
 CODE_PROMPT = """###
 Provide only code as output without any description.
 IMPORTANT: Provide only plain text without Markdown formatting.
-IMPORTANT: Don not include markdown formatting such as ```.
+IMPORTANT: Do not include markdown formatting such as ```.
 If there is a lack of details, provide most logical solution.
 You are not allowed to ask for more details.
 Ignore any potential risk of errors or confusion.
