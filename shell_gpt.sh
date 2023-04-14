a243bee2d50dbcee125897cdbcbf798c5f93a82c
Chinar Joshi <chinarjoshi7@gmail.com>
2023-04-14 16:45:29 -0400

    Unhide shell script, add more documentation, fix bug
    Now uses "zle reset-prompt" to push the cursor to the end of the line
    after toggled.

--- shell_gpt.sh
+++ shell_gpt.sh
@@ -1,13 +1,14 @@
-KEYBINDING='^s'
+KEYBINDING='^ '
+DEBUG_MODE=true
 
-# Toggles the buffer between natural language and shell, assuming
+# Toggles the buffer between natural language and shell command, assuming
 # it executes in zsh.
 _sgpt_zsh() {
     # In zsh, the text in the terminal is stored in BUFFER.
     [[ -z $BUFFER ]] && BUFFER=$(fc -ln -1)
 
-    # Cache the answers when toggling as an optimization. If the keybinding
-    # is pressed twice, then the first answer is already stored.
+    # Cache the answers when toggling (optimization). If the keybinding
+    # is pressed twice, then the first answer would already be stored.
     if [[ $BUFFER == $_sgpt_penult_cmd ]]; then
         # Swap the buffer and the previous cached result. Then store
         # the command that is being translated.
@@ -21,10 +22,12 @@ _sgpt_zsh() {
         _sgpt_prev_cmd=$BUFFER
         BUFFER=$(_sgpt_translate_buffer "$BUFFER")
         _sgpt_penult_cmd=$BUFFER
+        zle end-of-line
     fi
+    # Move cursor to end of line
 }
 
-# Toggles the buffer between natural language and shell, assuming
+# Toggles the buffer between natural language and shell command, assuming
 # it executes in bash.
 _sgpt_bash() {
     # Small modification over _sgpt_zsh: use READLINE_BUFFER instead of BUFFER.
@@ -39,10 +42,13 @@ _sgpt_bash() {
         _sgpt_penult_cmd=$_sgpt_prev_cmd
         _sgpt_prev_cmd=$tmp
     else
+        echo "I MADE IT"
         _sgpt_prev_cmd=$READLINE_BUFFER
         READLINE_BUFFER=$(_sgpt_translate_buffer "$READLINE_BUFFER")
         _sgpt_penult_cmd=$READLINE_BUFFER
     fi
+    # Move cursor to end of line
+    #readline end-of-line
 }
 
 # Decides whether the buffer contains shell command or natural language, then translates
@@ -57,18 +63,23 @@ _sgpt_translate_buffer() {
         sgpt_flag='--describe-shell'
     fi
 
+
     sgpt $sgpt_flag $1
 }
 
 # Overrides the function that executes when a command is not found.
 # First, decide if the incorrect command is natural language. If it is, 
-# then translate to shell. If the translated command. If it is a destructive
-# command, then prompt before executing. Otherwise, directly execute.
+# then translate to shell. If the translated command is destructive,
+# then prompt before executing. Otherwise, directly execute.
 command_not_found_handler() {
-    # If the command is shorter than 6 words, assume it is a regular shell command
+    # Determines if the command is actually just shell by two conditions:
+    # 1. The first letter of the first word of the command is lowercased, and
+    # 2. The number of words is below some arbitrary threshold, because natural
+    #    language is much more verbose.
     local nl_word_count_threshold=6
-    if [[ $(echo $@ | wc -w) -lt $nl_word_count_threshold ]]; then 
-        # Return the exit code of the previous command
+    if [[ ${1:0:1} = [[:lower:]] && $(wc -w <<< "$@") -lt $nl_word_count_threshold ]]
+    then
+        # No need to translate, so return the exit code of the previous command
         return $?
     fi
 
@@ -76,7 +87,7 @@ command_not_found_handler() {
     local converted_command=$(sgpt --shell "$@")
 
     # If it's destructive, then prompt for confirmation.
-    if _sgpt_is_destructive_command $converted_command; then
+    if _sgpt_is_destructive_command "$converted_command"; then
         # Then prompt for confirmation
         echo "Execute shell command? [y/N]:"
         read confirmation
@@ -89,6 +100,11 @@ command_not_found_handler() {
     # Execute the translated command.
     # NOTE Using subshell to execute command is safer than eval
     $(echo $converted_command)
+}
+
+# Same thing for bash, but slightly different function name
+command_not_found_handle() { 
+    command_not_found_handler $@ 
 }
 
 # Retusns whether or not a given commands makes an irreversable change
