e364da29d4cf924b625eb716fd3a6084f64e8ed2
Farkhod Sadykov <farkhod.sadykov@gmail.com>
2023-06-04 20:40:20 +0200

    Fixing hotkey for bash shell

--- simple_bash.sh
+++ simple_bash.sh
@@ -3,5 +3,5 @@ _sgpt_bash() {
     READLINE_LINE=$(sgpt --shell <<< "$READLINE_LINE")
     READLINE_POINT=${#READLINE_LINE}
 }
-bind -x '^l' '_sgpt_bash'
+bind -x '"\C-l": _sgpt_bash'
 # Shell-GPT integration BASH v0.1
