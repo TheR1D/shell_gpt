fcd7530f3a2f298bbcec514a4532c28547e581ff
kyrbrbik <103336573+kyrbrbik@users.noreply.github.com>
2023-06-12 18:22:22 +0200

    Added a check for empty var (#272)

--- simple_zsh.sh
+++ simple_zsh.sh
@@ -1,10 +1,12 @@
 # Shell-GPT integration ZSH v0.1
 _sgpt_zsh() {
+if [[ -n "$BUFFER"]]; then
     _sgpt_prev_cmd=$BUFFER
     BUFFER+="⌛"
     zle -I && zle redisplay
     BUFFER=$(sgpt --shell <<< "$_sgpt_prev_cmd")
     zle end-of-line
+fi
 }
 zle -N _sgpt_zsh
 bindkey ^l _sgpt_zsh
