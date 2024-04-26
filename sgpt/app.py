2060f276b6d675f58a0d1ad42afb3bd5c18d940d
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-04-26 12:39:29 -0700

    Copy the last chat file over on new --follow-up flag

--- sgpt/app.py
+++ sgpt/app.py
@@ -5,8 +5,10 @@ import readline  # noqa: F401
 import sys
 
 import typer
+import shutil
 from click import BadArgumentUsage
 from click.types import Choice
+from pathlib import Path
 
 from sgpt.config import cfg
 from sgpt.function import get_openai_schemas
@@ -104,6 +106,13 @@ def main(
         help="Start a REPL (Read–eval–print loop) session.",
         rich_help_panel="Chat Options",
     ),
+    follow_up: bool = typer.Option(
+        False,
+        "--follow-up",
+        "-fu",
+        help="Follow up on last prompt, " "used with --repl and --chat",
+        rich_help_panel="Chat Options",
+    ),
     show_chat: str = typer.Option(
         None,
         help="Show all messages from provided chat id.",
@@ -191,6 +200,11 @@ def main(
     if chat and repl:
         raise BadArgumentUsage("--chat and --repl options cannot be used together.")
 
+    if follow_up and not (chat or repl):
+        raise BadArgumentUsage(
+            "--follow-up option can only be used with --chat or --repl."
+        )
+
     if editor and stdin_passed:
         raise BadArgumentUsage("--editor option cannot be used with stdin input.")
 
@@ -204,6 +218,9 @@ def main(
     )
 
     function_schemas = (get_openai_schemas() or None) if functions else None
+
+    if follow_up:
+        register_last_chat(chat or repl)
 
     if repl:
         # Will be in infinite loop here until user exits with Ctrl+C.
@@ -257,6 +274,22 @@ def main(
             )
             continue
         break
+
+
+def register_last_chat(new_chat_id: str):
+    """
+    Registers the last default chat as an actual chat in the ChatSessions.
+
+    This enables following up on one-off prompts.
+    """
+    last_chat = Path(cfg.get("CACHE_PATH")) / "last_chat"
+    if not last_chat.exists():
+        return
+    new_chat = Path(cfg.get("CHAT_CACHE_PATH")) / new_chat_id
+    if new_chat.exists():
+        # Don't overwrite existing chats
+        return
+    shutil.copy(last_chat, new_chat)
 
 
 def entry_point() -> None:
